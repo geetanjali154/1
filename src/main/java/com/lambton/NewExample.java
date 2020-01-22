0107b6c0daeace6491e0684ad8fae88c5ccb8b66
MacStudent <macstudent@128-iMAC07.local>
2020-01-21 20:51:20 -0500

    [geetanjali] Initial commit

---
+++ src/main/java/com/lambton/NewExample.java
@@ -0,0 +1,14 @@
+package com.lambton;
+
+public class NewExample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("welcome");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("geet");
+        }
+
+    }
+}
